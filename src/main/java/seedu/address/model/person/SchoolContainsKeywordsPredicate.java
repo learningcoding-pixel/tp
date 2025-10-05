95a623fd72f8890c4b89e6b56ffab7d19459811d
jordanloh <jordan.loh@outlook.com>
2025-10-05 15:03:13 +0800

    Add basic find functionality
    Find functionality now supports searching via prefixes.
    Currently supports name and school only, with School using simple String as placeholders.
    Role and Tag finding has not been implemented.

---
+++ src/main/java/seedu/address/model/person/SchoolContainsKeywordsPredicate.java
@@ -0,0 +1,41 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+
+public class SchoolContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public SchoolContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getSchool(), keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof NameContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        SchoolContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (SchoolContainsKeywordsPredicate) other;
+        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
