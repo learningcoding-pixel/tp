3947e2f4d684e477743b94b3a56ac52966811864
learningcoding-pixel <yinweihaowork@gmail.com>
2025-10-12 23:52:54 +0800

    update import

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -2,8 +2,16 @@ package seedu.address.testutil;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Height;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Role;
+import seedu.address.model.person.School;
+import seedu.address.model.person.Weight;
+import seedu.address.model.person.Dob;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -139,6 +147,6 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, school, role, height, weight, tags, dob); // added by WH
+        return new Person(name, phone, email, address, school, role, height, weight, tags, dob);
     }
 }
