fe4d3e2b4ad395b6b9b6103ae53431d2463aaefc
learningcoding-pixel <yinweihaowork@gmail.com>
2025-10-20 20:23:43 +0800

    Session class: add location, startDate and endDate

--- src/main/java/seedu/address/model/session/Session.java
+++ src/main/java/seedu/address/model/session/Session.java
@@ -13,29 +13,41 @@ import seedu.address.commons.util.ToStringBuilder;
  */
 public class Session {
 
-    protected LocalDateTime date;
+    protected LocalDateTime startDate;
+    protected LocalDateTime endDate;
     protected DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm");
     private final String session;
+    private final String location;
 
     /**
      * Represents a Session in the address book.
      * Guarantees: details are present and not null, field values are validated, immutable.
      */
-    public Session(String session, LocalDateTime date) {
+    public Session(String session, String location, LocalDateTime startDate, LocalDateTime EndDate) {
         this.session = session;
-        this.date = date;
+        this.location = location;
+        this.startDate = startDate;
+        this.endDate = EndDate;
     }
 
     public String getSession() {
         return session;
     }
 
-    public LocalDateTime getDate() {
-        return date;
+    public String getLocation() {
+        return location;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
     }
 
     /**
-     * Returns true if both sessions have the same name.
+     * Returns true if both sessions have the same session name.
      * This defines a weaker notion of equality between two persons.
      */
     public boolean isSameSession(Session otherSession) {
@@ -64,13 +76,15 @@ public class Session {
 
         Session otherSession = (Session) other;
         return session.equals(otherSession.session)
-                && date.equals(otherSession.date);
+                && location.equals(otherSession.location)
+                && startDate.equals(otherSession.startDate)
+                && endDate.equals(otherSession.endDate);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(session, date);
+        return Objects.hash(session, location, startDate, endDate);
     }
 
 
@@ -78,7 +92,9 @@ public class Session {
     public String toString() {
         return new ToStringBuilder(this)
                 .add("Session", session)
-                .add("Date", date.format(formatter))
+                .add("Location",location)
+                .add("StartDate", startDate.format(formatter))
+                .add("EndDate", endDate.format(formatter))
                 .toString();
     }
 }
