f3ef57209c31ab7e86c635ee5bb755c21e7544f6
Jireh <tohjireh9@gmail.com>
2025-10-21 22:14:59 +0800

    Make team card length longer

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -12,6 +12,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
 
+/**
+ * A UI component that displays information of a {@code Person}.
+ */
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
