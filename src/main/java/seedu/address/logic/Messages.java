9a945436842518ce733ec47283d717b90d122ef0
learningcoding-pixel <yinweihaowork@gmail.com>
2025-10-26 12:36:47 +0800

    Add FindTeam feature

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -21,6 +21,8 @@ public class Messages {
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
 
+    public static final String MESSAGE_TEAMS_LISTED_OVERVIEW = "%1$d athletes listed!";
+
     /**
      * Returns an error message indicating the duplicate prefixes.
      */
---
+++ src/main/java/seedu/address/logic/commands/FindTeamCommand.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.team.TeamNameContainsKeywordsPredicate;
+
+
+
+public class FindTeamCommand extends Command{
+
+    public static final String COMMAND_WORD = "findteam";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all Teams whose names contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " starteam pineapple amazingteam";
+
+    private final TeamNameContainsKeywordsPredicate predicate;
+
+    public FindTeamCommand(TeamNameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredTeamList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_TEAMS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FindCommand)) {
+            return false;
+        }
+
+        FindTeamCommand otherFindTeamCommand = (FindTeamCommand) other;
+        return predicate.equals(otherFindTeamCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+
+}
