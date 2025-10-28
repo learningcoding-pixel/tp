cc740a91101fa75ce4aea224ee98ca74f4076982
jordanloh <jordan.loh@outlook.com>
2025-10-28 19:39:26 +0800

    Update FindCommand to have clearer output messages

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -151,6 +151,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public boolean isAddressBookEmpty() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
