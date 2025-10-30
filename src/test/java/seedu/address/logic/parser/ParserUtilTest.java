66b3c8abd9a953257713df96929384a4b88a7391
yycancode <111871106+yycancode@users.noreply.github.com>
2025-10-30 17:23:28 +0800

    More Test Cases for ParserUtil

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -10,15 +10,24 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Dob;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Height;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Role;
+import seedu.address.model.person.School;
+import seedu.address.model.person.Weight;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.team.TeamName;
+import seedu.address.model.team.session.Location;
 
 public class ParserUtilTest {
     private static final String INVALID_NAME = "R@chel";
@@ -35,6 +44,22 @@ public class ParserUtilTest {
     private static final String VALID_TAG_2 = "neighbour";
 
     private static final String WHITESPACE = " \t\r\n";
+
+    private static final String INVALID_DOB = "10/10/2000"; // wrong format
+    private static final String INVALID_ROLE = " ";
+    private static final String INVALID_SCHOOL = " ";
+    private static final String INVALID_HEIGHT = "abc";
+    private static final String INVALID_WEIGHT = "abc";
+    private static final String INVALID_TEAM_NAME = " ";
+    private static final String INVALID_LOCATION = " ";
+
+    private static final String VALID_DOB = "2000-10-10";
+    private static final String VALID_ROLE = "Runner";
+    private static final String VALID_SCHOOL = "NUS";
+    private static final String VALID_HEIGHT = "170";
+    private static final String VALID_WEIGHT = "65";
+    private static final String VALID_TEAM_NAME_STR = "Alpha";
+    private static final String VALID_LOCATION_STR = "Track";
 
     @Test
     public void parseIndex_invalidInput_throwsParseException() {
@@ -146,6 +171,159 @@ public class ParserUtilTest {
         String emailWithWhitespace = WHITESPACE + VALID_EMAIL + WHITESPACE;
         Email expectedEmail = new Email(VALID_EMAIL);
         assertEquals(expectedEmail, ParserUtil.parseEmail(emailWithWhitespace));
+    }
+
+    @Test
+    public void parseDob_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseDob((String) null));
+    }
+
+    @Test
+    public void parseDob_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseDob(INVALID_DOB));
+    }
+
+    @Test
+    public void parseDob_validValueWithAndWithoutWhitespace_returnsDob() throws Exception {
+        Dob expected = new Dob(VALID_DOB);
+        assertEquals(expected, ParserUtil.parseDob(VALID_DOB));
+        String withWs = WHITESPACE + VALID_DOB + WHITESPACE;
+        assertEquals(expected, ParserUtil.parseDob(withWs));
+    }
+
+    @Test
+    public void parseRole_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseRole((String) null));
+    }
+
+    @Test
+    public void parseRole_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseRole(INVALID_ROLE));
+    }
+
+    @Test
+    public void parseRole_validValue_returnsRole() throws Exception {
+        Role expected = new Role(VALID_ROLE);
+        assertEquals(expected, ParserUtil.parseRole(VALID_ROLE));
+    }
+
+    @Test
+    public void parseSchool_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseSchool((String) null));
+    }
+
+    @Test
+    public void parseSchool_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseSchool(INVALID_SCHOOL));
+    }
+
+    @Test
+    public void parseSchool_validValue_returnsSchool() throws Exception {
+        School expected = new School(VALID_SCHOOL);
+        assertEquals(expected, ParserUtil.parseSchool(VALID_SCHOOL));
+    }
+
+    @Test
+    public void parseHeight_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseHeight((String) null));
+    }
+
+    @Test
+    public void parseHeight_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseHeight(INVALID_HEIGHT));
+    }
+
+    @Test
+    public void parseHeight_validValue_returnsHeight() throws Exception {
+        Height expected = new Height(VALID_HEIGHT);
+        assertEquals(expected, ParserUtil.parseHeight(VALID_HEIGHT));
+    }
+
+    @Test
+    public void parseWeight_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseWeight((String) null));
+    }
+
+    @Test
+    public void parseWeight_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseWeight(INVALID_WEIGHT));
+    }
+
+    @Test
+    public void parseWeight_validValue_returnsWeight() throws Exception {
+        Weight expected = new Weight(VALID_WEIGHT);
+        assertEquals(expected, ParserUtil.parseWeight(VALID_WEIGHT));
+    }
+
+    @Test
+    public void parseTeamName_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseTeamName((String) null));
+    }
+
+    @Test
+    public void parseTeamName_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseTeamName(INVALID_TEAM_NAME));
+    }
+
+    @Test
+    public void parseTeamName_validValue_returnsTeamName() throws Exception {
+        TeamName expected = new TeamName(VALID_TEAM_NAME_STR);
+        assertEquals(expected, ParserUtil.parseTeamName(VALID_TEAM_NAME_STR));
+    }
+
+    @Test
+    public void parseIndexes_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseIndexes(null));
+    }
+
+    @Test
+    public void parseIndexes_collectionWithInvalid_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseIndexes(Arrays.asList("1", "a")));
+    }
+
+    @Test
+    public void parseIndexes_collectionWithValid_returnsSet() throws Exception {
+        Set<Index> set = ParserUtil.parseIndexes(Arrays.asList("1", "2", "3"));
+
+        java.util.Set<Integer> oneBased = set.stream()
+                .map(Index::getOneBased)
+                .collect(java.util.stream.Collectors.toSet());
+
+        assertEquals(new java.util.HashSet<>(java.util.Arrays.asList(1, 2, 3)), oneBased);
+        assertEquals(3, set.size());
+    }
+
+    @Test
+    public void parseLocation_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseLocation((String) null));
+    }
+
+    @Test
+    public void parseLocation_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseLocation(INVALID_LOCATION));
+    }
+
+    @Test
+    public void parseLocation_validValue_returnsLocation() throws Exception {
+        Location expected = new Location(VALID_LOCATION_STR);
+        assertEquals(expected, ParserUtil.parseLocation(VALID_LOCATION_STR));
+    }
+
+    @Test
+    public void parseDateTime_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseDateTime((String) null));
+    }
+
+    @Test
+    public void parseDateTime_invalidFormat_throwsParseException() {
+        // Contains a colon in minutes, which ParserUtil rejects (expects HHmm)
+        assertThrows(ParseException.class, () -> ParserUtil.parseDateTime("2025-10-21 07:00"));
+    }
+
+    @Test
+    public void parseDateTime_valid_returnsLocalDateTime() throws Exception {
+        LocalDateTime expected = LocalDateTime.of(2025, 10, 21, 7, 0);
+        assertEquals(expected, ParserUtil.parseDateTime("2025-10-21 0700"));
     }
 
     @Test
